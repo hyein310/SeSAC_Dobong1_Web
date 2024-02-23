db2e4a2ab00ef82bdf7e1cc31072b5b7fa9ad59a
hyein310 <kanghyein310@naver.com>
2024-02-23 12:00:06 +0900

    2024-02-23

--- 19_jwt/app.js
+++ 19_jwt/app.js
@@ -2,52 +2,81 @@ const express = require("express");
 const app = express();
 const PORT = 8080;
 const jwt = require("jsonwebtoken");
-const SECRET = "gYbQho2A9lIxHBGxhI72Tzrtvv1zrl" // 랜덤 문자열
+const SECRET = "pWeSQjT6y7z7yyR3EsZGC5TYMcpVNY"; // random string
 
-app.set('views', './views');
-app.set('view engine', 'ejs');
+app.set("view engine", "ejs");
+app.set("views", "./views");
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-userInfo = {id: 'cocoa', pw: '1234', name: '코코아', age: 18};
+const userInfo = { id: "cocoa", pw: "1234", name: "코코아", age: 18 };
 
-app.get('/',(req, res)=> {
-    res.render("index");
+app.get("/", (req, res) => {
+  res.render("index");
 });
-app.get('/login',(req, res)=> {
-    res.render("login");
+
+app.get("/login", (req, res) => {
+  res.render("login");
 });
 
 // 로그인 요청
 // jwt 생성
-app.post("/login", (req, res)=> {
-    try{
-    // console.log(req.body);
-    // res.send("서버 콘솔 확인");
+app.post("/login", (req, res) => {
+    try {
+      console.log(req.body);
+      const { id, pw } = req.body;
+      const { id: realId, pw: realPw } = userInfo;
 
-    const { id, pw } = req.body;
-    const { id: realId, pw: realPw } = userInfo;
-
-    if(id===realId && pw === realPw) {
-        // jwt 인증 토큰 생성
-        // const token = jwt.sign(payload, secret/private key, option);
-        const token = jwt.sign({id: id}, SECRET);
+      if (id === realId && pw === realPw) {
+        // jtw 인증 토큰 생성
+        // const token=jwt.sign(payload, secret/private key, option)
+        const token = jwt.sign({ id }, SECRET);
         console.log(token);
-        res.send({result:true, token: token}) // 키랑 값이랑 똑같다면 생략해서 하나로 표기해도 됨
+        res.send({ result: true, token });  // 키랑 값이랑 똑같다면 생략해서 표시해도 됨
+      } else {
+        res.send({ result: false, message: "로그인 정보가 올바르지 않습니다." });
+      }
+    } catch (err) {
+      console.log("POST /login ", err);
+      res.status(500).send("server error");
     }
-    else {
-        res.send({resulst:false, message: '로그인 정보가 올바르지 않습니다'});
-    }
-    }
-    catch (err) {
-        console.log("POST /login", err);
-        res.status(500).send("server error");
-    }
-});
+  });
 
 // token 정보 확인
-app.post("/token", (req, res)=> {
+app.post("/token", (req, res) => {
+    try {
+        console.log(req.headers.authorization);
+        // 인증 정보 들어왔을 때
+        // token만을 저장하기 위해서
+        if(req.headers.authorization) {
+        const token = req.headers.authorization.split(" ")[1];
+        try {
+            console.log("token >> ",token);
+            
+            const auth = jwt.verify(token, SECRET);
+            console.log("auth >> ",auth);
+            // verify()의 리턴값 >> { id: 'cocoa', iat: 1708656768 }
+            // iat: issued at, 발급된 시간
+
+            if(userInfo.id === auth.id){
+                res.send({result: true, name: userInfo.name})
+            }
+            res.end();
+        } catch (error) {
+            console.log("토큰 인증 에러", err);
+            // 잘못된 정보 들어왔을 때
+            res.send({result: false, message: "인증된 회원이 아닙니다."})
+        }
+        }
+        else {
+            // 인증정보 안들어왔을 때
+            res.redirect("/login");
+        }
+    } catch (error) {
+        console.log("POST /token", err);
+        res.status(500).send("server error");
+    }
 
 });
 
